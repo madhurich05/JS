982e8132283f88b3391e3851e32b9b337f7d6e5e
Madhuri Chadalapaka <madhuri.chadalapaka@gmail.com>
2019-07-15 13:52:36 -0700

    Update and rename LetterChange to LetterChange.js

--- LetterChange.js
+++ LetterChange.js
@@ -3,14 +3,17 @@
 // if it is z /Z, it should be replaced by a
 function letterChange(str) {
 
-    let newStr = str.replace(/[a-z]/gi, function (char) {
+    let newStr = str.toLowerCase().replace(/[a-z]/gi, function (char) {
         if (char === 'z' || char === 'Z') {
             return 'a';
         } else {
             return String.fromCharCode(char.charCodeAt() + 1);
         }
     });
+    newStr = newStr.replace(/a|e|i|o|u/gi , function(vowel){
+        return vowel.toUpperCase();
+    });
     return newStr;
 }
 
-letterChange("Hello There") // "Ifmmp Uifsf"
+letterChange("Hello There") // "Ifmmp UIfsf"

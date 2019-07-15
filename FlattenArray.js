4a91778ef985fd61d3415f16b62b84a1934239f4
Madhuri Chadalapaka <madhuri.chadalapaka@gmail.com>
2019-07-15 13:22:59 -0700

    Create FlattenArray.js

---
+++ FlattenArray.js
@@ -0,0 +1,7 @@
+function flatternArray(arrChunks) {
+
+    return [].concat.apply([],arrChunks);
+}
+
+arrChunks // (5) [Array(2), Array(2), Array(2), Array(2), Array(1)
+flatternArray(arrChunks); // (9) [2, 4, 6, 3, 6, 9, 4, 8, 12]

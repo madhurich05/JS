047308a4ecfb6cb00d281840e9225bdba2a2336f
Madhuri Chadalapaka <madhuri.chadalapaka@gmail.com>
2019-07-15 13:25:23 -0700

    Update FlattenArray.js

--- FlattenArray.js
+++ FlattenArray.js
@@ -2,6 +2,10 @@ function flatternArray(arrChunks) {
 
     return [].concat.apply([],arrChunks);
 }
+// Solution 2
+function flatternArray(arrChunks) {
 
+    return [].concat(...arrChunks);
+}
 arrChunks // (5) [Array(2), Array(2), Array(2), Array(2), Array(1)
 flatternArray(arrChunks); // (9) [2, 4, 6, 3, 6, 9, 4, 8, 12]
